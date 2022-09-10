8bf702ec5da2f66bf4ed5c8a2e1c3c857633918a
saadal <saadalmubaraksy@gmail.com>
2022-09-10 20:19:30 +0300

    add sample unit test

---
+++ Maqta.GateWay.EmployeeCRUD/employee.API.Test/EmployeeControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Maqta.GateWay.EmployeeCRUD.ApplicationContract;
+using Maqta.GateWay.EmployeeCRUD.HttpApi.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee.API.Test
+{
+    public class EmployeeControllerTest
+    {
+        private readonly EmployeesController _controller;
+        private readonly EmployeeServiceFake _service;
+        public EmployeeControllerTest()
+        {
+            _service = new EmployeeServiceFake();
+            _controller = new EmployeesController(_service);
+        }
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = await _controller.GetEmployee(1);
+            // Assert
+            Assert.IsType<EmployeeDto>(okResult as EmployeeDto);
+        }
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = await _controller.GetEmployee(2) as EmployeeDto;
+            // Assert
+            var items = Assert.IsType<List<EmployeeDto>>(okResult.FirstName);
+            Assert.Equal(3, items.Count);
+        }
+    }
+}
+
---
+++ Maqta.GateWay.EmployeeCRUD/employee.API.Test/EmployeeServiceFake.cs
@@ -0,0 +1,50 @@
+﻿using Maqta.GateWay.EmployeeCRUD.ApplicationContract;
+using Maqta.GateWay.EmployeeCRUD.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee.API.Test
+{
+    public class EmployeeServiceFake:IEmployeeService
+    {
+        private readonly List<EmployeeDto> _employees;
+        public EmployeeServiceFake()
+        {
+            _employees = new List<EmployeeDto>()
+            {
+                 new EmployeeDto() { Id=1,  FirstName= "test1", LastName = "test1Last", EmailID ="test1email" },
+                new EmployeeDto() { Id=2,  FirstName= "test2", LastName = "test2Last", EmailID ="test2email" },
+                 new EmployeeDto() { Id=3,  FirstName= "test3", LastName = "test3Last", EmailID ="test3email" },
+            };
+        }
+        public Task<List<EmployeeDto>> GetAllEmployeesAsync()
+        {
+            return Task.FromResult(_employees);
+        }
+        public Task<EmployeeDto> CreateEmployeeAsync(EmployeeDto newEmployee)
+        {
+            _employees.Add(newEmployee);
+            return Task.FromResult(newEmployee);
+        }
+        public Task<EmployeeDto> GetEmployeeAsync(int id)
+        {
+            return Task.FromResult(_employees.Where(a => a.Id == id)
+                .FirstOrDefault());
+        }
+        public Task<bool> DeleteEmployeeAsync(int id)
+        {
+            var existing = _employees.First(a => a.Id == id);
+            _employees.Remove(existing);
+            return Task.FromResult(true);
+        }
+
+
+        public Task<bool> UpdateEmployeeAsync(EmployeeDto employee, int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
